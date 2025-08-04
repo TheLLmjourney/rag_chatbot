b19fac734bbeec39dd72755c0c46c252436b2585
TheLLmjourney <atharvyeram11@gmail.com>
2025-08-04 08:11:55 +0000

    Setup and requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='rag_chatbot',
+  version='0.1',
+  author='Atharv Yeram',
+  author_email='atharvyeram11@gmail.com',
+  packages=find_packages(),
+  install_requires=[]
+)
